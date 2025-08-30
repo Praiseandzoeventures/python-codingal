dd2d6040a9ada580e11719417de671863ec95b32
Praiseandzoeventures <onyfaith@gmail.com>
2025-08-30 16:36:29 +0200

    lesson 1

---
+++ lesson 1/print statement .py
@@ -0,0 +1,8 @@
+#printing greeting message
+print("hello world")
+#printing my name
+print ("my name is praise")
+#here are fun fact about me
+print  ("i like to read and draw")
+#calculation
+print  ("the result of 3+5= ",3+5)
