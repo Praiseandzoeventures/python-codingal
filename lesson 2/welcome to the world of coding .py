788468d455251f16ee7b91762be392dcefd3939f
Praiseandzoeventures <onyfaith@gmail.com>
2025-08-31 16:28:11 +0200

    lesson2

---
+++ lesson 2/welcome to the world of coding .py
@@ -0,0 +1,10 @@
+# print a sentence 
+print ("welcome to the world of progaramming")
+
+print (279)
+
+print ("hello python \n")
+
+print ("hello",279153)
+
+print ("welcome to " , end= "*")
---
+++ python .py
@@ -0,0 +1,11 @@
+x =5
+y ="Jhon"
+
+print(x)
+print(y)
+
+codingal="coding"
+print (codingal)
+
+name=input("enter your name :")
+print("\n hello ",name,"\n welcome to codingal")
