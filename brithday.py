fec85be99d19bc2f4dd4cf0cc7c10186bdb5c065
Praiseandzoeventures <onyfaith@gmail.com>
2025-08-31 17:11:50 +0200

    brithday

---
+++ brithday.py
@@ -0,0 +1,14 @@
+praise=27|11|25
+print("the brithday of praise is",praise )
+
+zoe=9|3|25
+print("the brithday of zoe is",zoe)
+
+faith=15|1|25
+print("the birthday of faith is",faith)
+
+peter=3|12|25
+print("the birthday of peter is",peter)
+
+wisdom=24|10|25
+print("the birthday of wisdom is",wisdom)
