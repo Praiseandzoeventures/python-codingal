37624974c994db2642f2970ade9787bc454e8b11
Praiseandzoeventures <onyfaith@gmail.com>
2025-09-07 16:28:47 +0200

    codes

---
+++ lesson 3/lesson 4/arerage.py
@@ -0,0 +1,11 @@
+tree1=900
+tree2=941
+tree3=415
+tree4=957
+tree5=11000
+
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum is",sum)
+average=sum/5
+
+print("the sum of the average",average)
---
+++ lesson 3/lesson 4/clacution.py
@@ -0,0 +1,22 @@
+print("Enter marks of four subjects:")
+
+math = int(input("enter maths marks:"))
+
+english = int(input("enter english marks:"))
+
+science = int(input("enter science marks:"))
+
+social = int(input("enter social marks:"))
+
+sum = math + english +science +social
+
+print("the sum of all subjects are:",sum)
+
+percentage = (sum/400) * 100
+
+print("The percentage is:",percentage)
+
+
+
+
+
