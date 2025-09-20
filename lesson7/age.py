abcf76443c56da47bc14a33cbcdea585350389eb
Praiseandzoeventures <onyfaith@gmail.com>
2025-09-20 17:11:01 +0200

    age

---
+++ lesson7/age.py
@@ -0,0 +1,12 @@
+age= input("are you under 10 or 20  Y or N")
+age= int(input("enter your age"))
+
+if age=='y':
+    print("you are allowed")
+else:
+    if age>=20:
+        print("allowed")
+        
+    else:
+        print("not allowed")
+    
