e6d84712f90a7243a811f4582259b8e89aadc423
Praiseandzoeventures <onyfaith@gmail.com>
2025-09-28 16:29:15 +0200

    mid

---
+++ lesson7/lesson 8/lesson9/lesson 10/character occurrence.py
@@ -0,0 +1,13 @@
+string=input("please enter the word")
+
+char=input("please enter your character ")
+
+i=0
+count=0
+
+while i<len(string):
+    if string[i]==char:
+        count=count+1
+    i=i+1
+    
+print("the character occurence is:",count)
