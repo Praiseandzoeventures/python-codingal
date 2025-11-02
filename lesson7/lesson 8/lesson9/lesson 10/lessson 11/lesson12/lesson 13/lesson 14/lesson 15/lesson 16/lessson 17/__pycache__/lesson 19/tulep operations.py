178c55536771536ce796bc2a9dc29a32f4530b89
Praiseandzoeventures <onyfaith@gmail.com>
2025-11-02 15:09:50 +0100

    rainy

---
+++ lesson7/lesson 8/lesson9/lesson 10/lessson 11/lesson12/lesson 13/lesson 14/lesson 15/lesson 16/lessson 17/__pycache__/lesson 19/tulep operations.py
@@ -0,0 +1,16 @@
+tuple1=("tuple" ,False,3,2,1)
+print(tuple1)
+
+tuple2=(4,6,2,8,3,1)
+print(tuple2)
+tuple2=tuple2+(9,8,7,)
+print(tuple2)
+
+tuple3=(50,10,60,70,50)
+print(tuple3.count(50))
+
+tuple4=(2,4,3,5,4,6,7,8,6,1)
+slice=tuple4[3:5]
+print(slice)
+slice2=tuple4[:6]
+print(slice2)
