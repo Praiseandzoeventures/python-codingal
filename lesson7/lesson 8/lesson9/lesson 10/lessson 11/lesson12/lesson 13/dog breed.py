e3d98fe9c644b34702bf7a90ab2de300c72c0c0b
Praiseandzoeventures <onyfaith@gmail.com>
2025-11-22 16:31:29 +0100

    dog

---
+++ lesson7/lesson 8/lesson9/lesson 10/lessson 11/lesson12/lesson 13/dog breed.py
@@ -0,0 +1,12 @@
+class dog:
+    def __init__(self,name,breed,age):
+        self.name=name
+        self.breed=breed
+        self.age=age
+        
+        dog1=dog("buddy,golden retriever",5)
+        dog2=dog("max,beagle",3)
+        
+print(dog.name,"is",dog.age,"years old")
+print(dog.name," is",dog.age,"years old")
+
