8fac63fd338860347a87124eb10ce4de0ff044f5
Praiseandzoeventures <onyfaith@gmail.com>
2026-01-17 15:29:21 +0100

    too

---
+++ lesson 25/lesson 29/lesson 30/lesson 33/lesson 34/lesson 35/lesson 36/lesson 37/homework/lesson 37/math.py/lesson 37/let's top a window.py
@@ -0,0 +1,15 @@
+from tkinter import*
+root=Tk()
+root.geometry("400x300")
+root.title("main")
+def topwin():
+    top=Toplevel()
+    top.geometry("100x100")
+    l2=Label(top,text="this is top level window")
+    l2.pack()
+    top.mainloop()
+l=Label(root,text="this is root window")
+btn=Button(root,text="clcik here to open another window",command=topwin)
+l.pack()
+btn.pack()
+root.mainloop()
