277232a9aa34d93a74cf27678ffdfaca3c743d12
Praiseandzoeventures <onyfaith@gmail.com>
2026-01-18 15:23:31 +0100

    ply

--- lesson 25/lesson 29/lesson 30/lesson 33/lesson 34/lesson 35/lesson 36/lesson 37/homework/lesson 37/math.py/mat.py
+++ lesson 25/lesson 29/lesson 30/lesson 33/lesson 34/lesson 35/lesson 36/lesson 37/homework/lesson 37/math.py/mat.py
@@ -5,3 +5,14 @@ x=10
 y=50
 if(x**2>100 and y<100):
     print(x,y)
+    
+    
+    
+    
+    num=7
+if num > 3:
+        print('3')
+if num > 5:
+        print('5')
+if num ==7 :
+        print('7')
